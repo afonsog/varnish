e59cb46b4a6b78c01180704e8fa70d3a33787074
Gerónimo <geronimo.afonso@mikroways.net>
2016-12-15 12:20:03 -0300

    script start.sh edited, option -F removed on varnishd

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 /confd-onetime-rancher
-varnishd -F -a :80 -T localhost:6082 -s file,/tmp/varnish_storage.bin,1G -f /etc/varnish/default.vcl -p thread_pool_min=200 -p thread_pool_max=2000 -p thread_pools=8 -p listen_depth=4096 -p lru_interval=60
+varnishd -a :80 -T localhost:6082 -s file,/tmp/varnish_storage.bin,1G -f /etc/varnish/default.vcl -p thread_pool_min=200 -p thread_pool_max=2000 -p thread_pools=8 -p listen_depth=4096 -p lru_interval=60
 /confd-rancher
 #CMD ./confd-onetime-rancher && varnishd -F -a :80 -T localhost:6082 -s file,/tmp/varnish_storage.bin,1G -f /etc/varnish/default.vcl -p thread_pool_min=200 -p thread_pool_max=2000 -p thread_pools=8 -p listen_depth=4096 -p lru_interval=60 && ./confd-rancher
